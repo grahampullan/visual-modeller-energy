0bd58e22785f790425a18db8c7d50f3d8060675e
Graham Pullan <gp10006@cam.ac.uk>
2024-09-23 03:24:42 +0100

    updated packages

--- build/visual-modeller-energy.js
+++ build/visual-modeller-energy.js
@@ -12058,7 +12058,15 @@ class LogViewer extends Plot {
                 gX = barChartSvg.append("g")
                     .attr("class", "x-axis")
                     .attr("transform", `translate(0, ${height})`)
-                    .call(xAxis);
+                    .call(xAxis)
+                    .selectAll("text")
+                        .attr("y", 0)
+                        .attr("x", 0)
+                        .attr("dy", ".35em")
+                        .attr("transform", "translate(0, 10)")  // Adjust vertical positioning
+                        .style("text-anchor", "middle")
+                        .call(wrapText, x.bandwidth());
+                        
             } else {
                 gX.call(xAxis);
             }
@@ -12080,8 +12088,34 @@ class LogViewer extends Plot {
             selectedLog.state = {name: null, targetName: null};
         }
 
+        function wrapText(text, width) {
+            text.each(function () {
+                const textElement = select(this);
+                const words = textElement.text().split(/\s+/).reverse();
+                let word,
+                    line = [],
+                    lineNumber = 0,
+                    lineHeight = 1.1, // ems
+                    y = textElement.attr("y"),
+                    dy = parseFloat(textElement.attr("dy")),
+                    tspan = textElement.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", `${dy}em`);
 
-    
+                while ((word = words.pop())) {
+                    line.push(word);
+                    tspan.text(line.join(" "));
+                    if (tspan.node().getComputedTextLength() > width) {
+                        line.pop();
+                        tspan.text(line.join(" "));
+                        line = [word];
+                        tspan = textElement.append("tspan")
+                            .attr("x", 0)
+                            .attr("y", y)
+                            .attr("dy", `${++lineNumber * lineHeight + dy}em`)
+                            .text(word);
+                    }
+                }
+            });
+        }
     }
 
     highlightLog(data) {
@@ -12111,8 +12145,6 @@ class LogViewer extends Plot {
         const iconContainer = container.select(".plot-icons");
         const lineIcon = iconContainer.select("#line-icon");
         const barIcon = iconContainer.select("#bar-icon");
-        this.linePlot;
-        this.barPlot;
         if (this.linePlot) {
             lineIcon.style("color", "black");
             barIcon.style("color", "gray");
